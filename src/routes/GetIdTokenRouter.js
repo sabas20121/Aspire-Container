a8747b7a42fddf581fdc331c342373dd977d462b
sahanawaz.khan <c-sahanawaz.khan@metricstream.com>
2023-12-07 12:41:00 +0530

    Updated Login Ui

--- src/routes/GetIdTokenRouter.js
+++ src/routes/GetIdTokenRouter.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const cognitoIdTokenController = require('../controllers/CognitoIdTokenController');
+const cognitoIdTokenController = require('../controllers/CognitoIdentityController');
 const authorize = require('../authorization/Authorization.js');
 
 router.post('/', authorize, cognitoIdTokenController.getIdToken);
